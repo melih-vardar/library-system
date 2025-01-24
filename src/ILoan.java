45119921dbc72d1a3c84e5f6fa30480d58d79b27
Melih Vardar <mvardar97@gmail.com>
2025-01-25 01:42:53 +0300

    ILoan class deleted

--- src/ILoan.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public interface ILoan {
-
-    void loanBook(Book book);
-    void loanBooks(List<Book> books);
-    void returnBook(Book book);
-    void returnBooks(List<Book> books);
-}
